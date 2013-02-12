6fa14c48611c10ff3218f2de41a548bce656fad8
Tom Nealon <tgnealon@gmail.com>
2013-02-12 03:18:51 +0000

    Local player implementation.

--- src/dominoes/players/LocalPlayer.java
+++ src/dominoes/players/LocalPlayer.java
@@ -5,13 +5,16 @@
 package dominoes.players;
 
 import dominoes.*;
+import java.util.ArrayList;
 
 /**
  *
  * @author tom
  */
 public abstract class LocalPlayer implements DominoPlayer {
-    
+    ArrayList<Bone> playerHand = new ArrayList<>();
+    String playerName = "";
+    int playerPoints = 0;
     
     @Override
     public Play makePlay(Table table) throws CantPlayException {
@@ -22,16 +25,50 @@ public abstract class LocalPlayer implements DominoPlayer {
     
     @Override
     public void takeBack(Bone bone) {
-         
+         // ?
     }
     
+    @Override
     public void draw(BoneYard boneyard) {
-        
+        Bone newBone = boneyard.draw();
+        if(newBone != null) {
+            playerHand.add(newBone);
+        }
     }
     
     @Override
     public int numInHand() {
-        
+        return playerHand.size();
+    }
+    
+    @Override
+    public Bone[] bonesInHand() {
+        return (Bone[]) playerHand.toArray();
+    }
+    
+    @Override
+    public void newRound() {
+        playerHand.clear();
+    }
+    
+    @Override
+    public void setPoints(int points) {
+        playerPoints = points;
+    }
+    
+    @Override
+    public int getPoints() {
+        return playerPoints;
+    }
+    
+    @Override
+    public void setName(String name) {
+        playerName = name;
+    }
+    
+    @Override
+    public String getName() {
+        return playerName;
     }
     
 }
